1451ab68adcdfa363ec75b73186d475f096acc51
Wellington Roth <wellingtonrothh@gmail.com>
2023-04-07 10:39:34 -0300

    home desktop layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,35 +1,87 @@
+<script setup></script>
+
 <template>
-  <div>
-    <header class="shadow-sm bg-white">
-      <nav class="container mx-auto p-4 flex justify-between">
-        <NuxtLink to="/" class="font-bold">Nuxt Dojo</NuxtLink>
-        <ul class="flex gap-4">
-          <li><NuxtLink to="/">Home</NuxtLink></li>
-          <li><NuxtLink to="/about">About</NuxtLink></li>
-          <li><NuxtLink to="/blog" class="btn">blog</NuxtLink></li>
+  <div class="main">
+    <header>
+      <nav class="navigation">
+        <ul class="list">
+          <li>
+            <NuxtLink to="/" class="item">
+              Início
+            </NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/about" class="item">
+              Sobre
+            </NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/blog" class="item">
+              Blog
+            </NuxtLink>
+          </li>
         </ul>
       </nav>
     </header>
 
     <!-- output the page content -->
-    <div class="container mx-auto p-4">
+    <section class="content">
       <slot />
-    </div>
+    </section>
 
-    <footer class="container mx-auto p-4 flex justify-between border-t-2">
-      <ul class="flex gap-4">
-        <li><NuxtLink to="/">Home</NuxtLink></li>
-        <li><NuxtLink to="/about">About</NuxtLink></li>
-        <li><NuxtLink to="/blog" class="btn">blog</NuxtLink></li>
+    <footer class="footer">
+      <div>
+        <img class="mix-blend-darken" src="../assets/images/wroth.jpg" alt="wellington roth" width="100px" height="100px">
+      </div>
+      <ul class="socials-list">
+        <li>
+          <a href="https://github.com/Wellingtonroth" target="_blank">
+            <img width="30px" height="30px" src="../assets/images/github.png" alt="github" />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/wellingtonroth/" target="_blank">
+            <img width="30px" height="30px" src="../assets/images/linkedin.png" alt="linkedin" />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/wellrothh/" target="_blank">
+            <img width="30px" height="30px" src="../assets/images/instagram.png" alt="instagram" />
+          </a>
+        </li>
       </ul>
     </footer>
   </div>
 </template>
 
-<script setup></script>
-
 <style scoped>
 .router-link-exact-active {
-  color: red;
+  @apply text-yellow-500 border-b-[1px] border-yellow-500;
+}
+
+.main {
+  @apply flex flex-col justify-between h-[100vh];
+}
+
+.navigation {
+  @apply main-container pt-10 pb-10 flex justify-center;
+}
+.list {
+  @apply flex gap-4 font-medium;
+}
+.item {
+  @apply pb-1 hover:text-yellow-400;
+}
+
+.content {
+  @apply main-container flex pb-4 h-full;
+}
+
+.footer {
+  @apply main-container flex justify-between py-10 border-t-[1px] border-black;
+}
+
+.socials-list {
+  @apply flex gap-4;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,108 @@
-<template>
-  <section>
-    <div>HOME! sou o index.vue</div>
-  </section>
-</template>
-
 <script setup></script>
 
-<style scoped></style>
+<template>
+  <div class="box">
+    <div class="presentation">
+      <img src="../assets/images/avatar.png" alt="wellington roth" width="300px" height="300px">
+      <span class="name">Wellington Roth</span>
+    </div>
+    <div class="presentation">
+      <p>
+        Lorem ipson dolar sit met Lorem ipson dolar sit met Lorem ipson dolar sit met Lorem ipson dolar sit met 
+        Lorem ipson dolar sit met Lorem ipson dolar sit met Lorem ipson dolar sit met Lorem ipson dolar sit met 
+        Lorem ipson dolar sit met Lorem ipson dolar sit met Lorem ipson dolar sit met Lorem ipson dolar sit met 
+        
+        Lorem ipson dolar sit met Lorem ipson dolar sit met Lorem ipson dolar sit met Lorem ipson dolar sit met 
+        Lorem ipson dolar sit met Lorem ipson dolar sit met Lorem ipson dolar sit met Lorem ipson dolar sit met 
+        Lorem ipson dolar sit met Lorem ipson dolar sit met Lorem ipson dolar sit met Lorem ipson dolar sit met 
+      </p>
+    </div>
+  </div>
+
+  <div class="box post">
+    <h3 class="last-posts">Ultimos posts</h3>
+    <div class="flex mt-[20px]">
+      <div style="width: 280px; height: 154px; background-color: grey"></div>
+      <div class="content">
+        <h4 class="title">Título do post</h4>
+        <p>Descição do post Lorem ipson dolar sit met Lorem ipson dolar</p>
+        <div class="tags">
+          <span class="tag">css</span>
+          <span class="tag">html</span>
+          <span class="tag">javascript</span>
+        </div>
+      </div>
+    </div>
+
+    <div class="flex mt-[20px]">
+      <div style="width: 280px; height: 154px; background-color: grey"></div>
+      <div class="content">
+        <h4 class="title">Título do post</h4>
+        <p>Descição do post Lorem ipson dolar sit met Lorem ipson dolar</p>
+        <div class="tags">
+          <span class="tag">css</span>
+          <span class="tag">html</span>
+          <span class="tag">javascript</span>
+        </div>
+      </div>
+    </div>
+
+    <div class="flex mt-[24px]">
+      <div style="width: 280px; height: 154px; background-color: grey"></div>
+      <div class="content">
+        <h4 class="title">Título do post</h4>
+        <p>Descição do post Lorem ipson dolar sit met Lorem ipson dolar</p>
+        <div class="tags">
+          <span class="tag">css</span>
+          <span class="tag">html</span>
+          <span class="tag">javascript</span>
+        </div>
+      </div>
+    </div>
+    <NuxtLink to="/blog" class="more">
+      Ver mais
+    </NuxtLink>
+  </div>
+</template>
+
+<style scoped>
+.box {
+  @apply flex w-2/4 h-full;
+}
+
+.presentation {
+  @apply flex flex-col justify-center items-center w-2/4;
+}
+
+.name {
+  @apply text-[24px];
+}
+
+.post {
+  @apply flex-col justify-center ml-[30px];
+}
+
+.last-posts {
+  @apply text-[18px] font-medium;
+}
+
+.content {
+  @apply flex flex-col justify-between ml-[26px];
+}
+
+.title{
+  @apply text-[18px] font-bold;
+}
+
+.tags {
+  @apply text-[12px] uppercase font-medium;
+}
+
+.tag {
+  @apply mr-[6px] text-[#023859];
+}
+
+.more {
+  @apply w-fit text-[12px] uppercase font-medium text-[#023859] mt-[24px];
+}
+</style>
