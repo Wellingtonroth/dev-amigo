f7467deef79d730a98adcc50e2c5d00895043e24
Wellington Roth <wellingtonrothh@gmail.com>
2023-04-10 16:13:04 -0300

    added color-mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,3 @@
-// // https://nuxt.com/docs/api/configuration/nuxt-config
-// export default defineNuxtConfig({
-//   modules: ['@nuxt/content', '@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
-//   colorMode: {
-//     classSuffix: '',
-//   },
-//   tailwindcss: {
-//     cssPath: '~/assets/css/tailwind.css',
-//     configPath: 'tailwind.config.js',
-//     exposeConfig: false,
-//     injectPosition: 0,
-//     viewer: true,
-//   },
-// });
-
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ['@nuxt/content', '@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
   colorMode: {
