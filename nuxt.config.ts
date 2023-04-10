19df03171db41fd731d1b2ed96d60a55e8e766de
Wellington Roth <wellingtonrothh@gmail.com>
2023-04-10 16:12:43 -0300

    config nuxt.config to implement darkmode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,29 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// // https://nuxt.com/docs/api/configuration/nuxt-config
+// export default defineNuxtConfig({
+//   modules: ['@nuxt/content', '@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
+//   colorMode: {
+//     classSuffix: '',
+//   },
+//   tailwindcss: {
+//     cssPath: '~/assets/css/tailwind.css',
+//     configPath: 'tailwind.config.js',
+//     exposeConfig: false,
+//     injectPosition: 0,
+//     viewer: true,
+//   },
+// });
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxt/content', '@nuxtjs/tailwindcss'],
+  modules: ['@nuxt/content', '@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
+  colorMode: {
+    classSuffix: ''
+  },
+  tailwindcss: {
+    cssPath: '~/assets/css/tailwind.css',
+    configPath: 'tailwind.config.js',
+    exposeConfig: false,
+    injectPosition: 0,
+    viewer: true,
+  },
 })
